9f44fe4729fd130404714b8bebaaf768adc746e5
fedebenelli <fedebenelli@outlook.com>
2021-08-17 22:18:58 -0300

    still testing, cp and w dont show desired results

--- gerg.f90
+++ gerg.f90
@@ -1,7 +1,7 @@
 ! Author= Federico Benelli
 ! Gerg-2008 Equation
 ! Started at: 01/07/2021
-! Last Modified: mar 27 jul 2021 15:57:12
+! Last Modified: mar 17 ago 2021 18:01:12
 !
 
 ! -------------------
@@ -20,10 +20,10 @@ Subroutine reducing_funcs(X, Bv, Gv, Bt, Gt, rho_c, rho_r, T_c, T_r)
     ! output:
     !  - rho_r  (float): Reducing density (1/rhor)
     !  - T_r    (float): reducing temperature (Tr)
-    double precision, dimension(21), intent(in):: X, rho_c, T_c
-    double precision, dimension(21, 21), intent(in)::  Bv, Gv, Bt, Gt
-    double precision, intent(inout):: rho_r, T_r
-    double precision:: eps = 0.0001
+    real*8, dimension(21), intent(in):: X, rho_c, T_c
+    real*8, dimension(21, 21), intent(in)::  Bv, Gv, Bt, Gt
+    real*8, intent(inout):: rho_r, T_r
+    real*8:: eps = 0.0001
     integer:: N = 21, i, j
 
     rho_r = 0
@@ -31,6 +31,7 @@ Subroutine reducing_funcs(X, Bv, Gv, Bt, Gt, rho_c, rho_r, T_c, T_r)
 
     do i = 1, N
     if (X(i) > eps) then
+        !write (0, *) i, rho_c(i), T_c(i)
         rho_r = rho_r + X(i)**2/rho_c(i)
         T_r = T_r + X(i)**2*T_c(i)
     end if
@@ -39,6 +40,7 @@ Subroutine reducing_funcs(X, Bv, Gv, Bt, Gt, rho_c, rho_r, T_c, T_r)
     do i = 1, N - 1
     do j = i + 1, N
     if (X(i) > eps .and. X(j) > eps) then
+        !write (0, *) i, j, Bv(i, j), Gv(i, j), Bt(i, j), Gt(i, j)
         rho_r = rho_r + &
                 2*X(i)*X(j)*Bv(i, j)*Gv(i, j) &
                 *(X(i) + X(j))/(Bv(i, j)**2*X(i) + X(j)) &
@@ -76,38 +78,27 @@ Subroutine a_oio(rho, T, rho_c, T_c, n, v, aoio)
     ! d2(aoio)/dd2  |     d2(aoio)/dt2 |    d2(aoio)/dddt |
     ! ----------------------------------------------------|
 
-    double precision, intent(in):: rho, T, rho_c, T_c
-    double precision, intent(in), dimension(7):: n, v
-    double precision, intent(inout):: aoio(3, 3)
-    double precision:: delta, tau
-    double precision:: r
+    real*8, intent(in):: rho, T, rho_c, T_c
+    real*8, intent(in), dimension(7):: n, v
+    real*8, intent(inout):: aoio(3, 3)
+    real*8:: r
 
     r = 8.314510/8.314472
 
     aoio = 0
-    delta = rho/rho_c
-    tau = T_c/T
-
-    aoio(1, 1) = log(delta) + r*(n(1) + n(2)*tau + n(3)*log(tau))
-    aoio(2, 1) = 1/delta
-    aoio(2, 2) = n(2) + n(3)/tau
-    aoio(3, 1) = -(1/delta)**2.0
-    aoio(3, 2) = n(3)*(T/T_c)**2
-    aoio(3, 3) = 0
 
     do k = 4, 6
-        aoio(1, 1) = aoio(1, 1) + r*n(k)*log(abs(sinh(v(k)*tau)))
-        aoio(2, 2) = aoio(2, 2) + n(k)*v(k)/tanh(v(k)*tau)
-        aoio(3, 2) = aoio(3, 2) + n(k)*v(k)**2/sinh(v(k)*tau)**2
+    if (v(k) > eps) then
+        aoio(3, 2) = aoio(3, 2) - n(k)*v(k)**2/(sinh(v(k)*T_c/T))**2
+    end if
     end do
     do k = 5, 7
-        aoio(1, 1) = aoio(1, 1) - r*n(k)*log(cosh(v(k)*tau))
-        aoio(2, 2) = aoio(2, 2) - n(k)*v(k)*tanh(v(k)*tau)
-        aoio(3, 2) = aoio(3, 2) + n(k)*v(k)**2/cosh(v(k)*tau)**2
+    if (v(k) > eps) then
+        aoio(3, 2) = aoio(3, 2) - n(k)*v(k)**2/(cosh(v(k)*T_c/T))**2
+    end if
     end do
 
-    aoio(2, 2) = r*aoio(2, 2)
-    aoio(3, 2) = -r*aoio(3, 2)
+    aoio(3, 2) = r*(-n(3)*(T/T_c)**2 + aoio(3, 2))
 
 End Subroutine a_oio
 
@@ -128,21 +119,24 @@ Subroutine a_oir(delta, tau, Kpol, Kexp, n, d, t, c, aoir)
     ! output:
     ! - aoir: Ideal Gas Helmholtz Energy and its derivatives,
     !           structured like:
-    ! ---------------+------------------+------------------|
+    ! ---------------+-------------------+-----------------|
     ! aoir           |      0            |   0             |
     ! d(aoir)/dd     |      d(aoir)/dt   |   0             |
     ! d2(aoir)/dd2   |      d2(aoir)/dt2 |   d2(aoir)/dddt |
     ! -----------------------------------------------------|
+    !
 
-    double precision, intent(in):: delta, tau
+    real*8, intent(in):: delta, tau
     integer, intent(in):: Kpol, Kexp
-    double precision, dimension(24), intent(in):: n, d, t, c
-    double precision, dimension(3, 3), intent(inout):: aoir
+    real*8, dimension(24), intent(in):: n, t
+    integer, dimension(24), intent(in):: d, c
+    real*8, dimension(3, 3), intent(inout):: aoir
     integer:: k
 
     aoir = 0
     ! Residual Helmholtz Energy
     do k = 1, Kpol
+        !write (0, *) k, n(k), d(k), t(k), c(k)
         aoir(1, 1) = aoir(1, 1) + &
                      n(k)*delta**d(k) &
                      *tau**t(k)
@@ -160,12 +154,16 @@ Subroutine a_oir(delta, tau, Kpol, Kexp, n, d, t, c, aoir)
                      *delta**(d(k) - 1)*tau**(t(k) - 1)
     end do
     do k = Kpol + 1, Kpol + Kexp
+        write (0, *) k, n(k), d(k), t(k), c(k)
         aoir(1, 1) = aoir(1, 1) + &
                      n(k)*delta**d(k) &
                      *tau**t(k) &
                      *exp(-delta**c(k))
         ! First Derivative with reduced density
-        aoir(2, 1) = aoir(2, 1) + n(k)*delta**(d(k) - 1)*(d(k) - c(k)*delta**c(k))*tau**t(k)*exp(-delta**c(k))
+        aoir(2, 1) = aoir(2, 1) + &
+                     n(k)*delta**(d(k) - 1) &
+                     *(d(k) - c(k)*delta**c(k)) &
+                     *tau**t(k)*exp(-delta**c(k))
         ! First Derivative with reduced temperature
         aoir(2, 2) = aoir(2, 2) + &
                      n(k)*t(k)*delta**d(k) &
@@ -218,15 +216,17 @@ Subroutine a_ijr(delta, tau, Kpolij, Kexpij, &
     ! d(aijr)/dd     |      d(aijr)/dt   |   0             |
     ! d2(aijr)/dd2   |      d2(aijr)/dt2 |   d2(aijr)/dddt |
     ! -----------------------------------------------------|
-    double precision, intent(in):: delta, tau
+    real*8, intent(in):: delta, tau
     integer, intent(in):: Kpolij, Kexpij
-    double precision, dimension(24), intent(in):: n, d, t, eta, eps, gamm, beta
-    double precision, dimension(3, 3), intent(inout):: aijr
+    real*8, dimension(24), intent(in):: n, t, eta, eps, gamm, beta
+    integer, dimension(24), intent(in):: d
+    real*8, dimension(3, 3), intent(inout):: aijr
     integer:: k
 
     aijr = 0
     ! Departure function
     do k = 1, Kpolij
+        !write (0, *) k, d(k), t(k), n(k), eta(k), eps(k), beta(k), gamm(k)
         aijr(1, 1) = aijr(1, 1) + &
                      n(k)*delta**d(k)*tau**t(k)
         aijr(2, 1) = aijr(2, 1) + &
@@ -243,6 +243,7 @@ Subroutine a_ijr(delta, tau, Kpolij, Kexpij, &
                      *delta**(d(k) - 1)*tau**(t(k) - 1)
     end do
     do k = Kpolij + 1, Kpolij + Kexpij
+        !write (0, *) k, d(k), t(k), n(k), eta(k), eps(k), beta(k), gamm(k)
         aijr(1, 1) = aijr(1, 1) + &
                      n(k)*delta**d(k)*tau**t(k) &
                      *exp( &
@@ -276,7 +277,6 @@ Subroutine a_ijr(delta, tau, Kpolij, Kexpij, &
                      -eta(k)*(delta - eps(k))**2 &
                      - beta(k)*(delta - gamm(k)) &
                      )
-        ! TODO esta derivada estan bien en el paper??
         aijr(3, 3) = aijr(3, 3) + &
                      n(k)*t(k)*delta**d(k)*tau**(t(k) - 1) &
                      *exp( &
@@ -289,23 +289,24 @@ End Subroutine a_ijr
 
 Subroutine ideal_term(X, rho, T, rho_c, T_c, rho_r, T_r, &
                       no, vo, ao)
-    double precision:: X(21), rho, T, rho_r, T_r
-    double precision, dimension(21, 7):: no, vo
-    double precision, dimension(3, 3):: ao, aoio
-    double precision, dimension(21):: rho_c, T_c
-    double precision:: eps = epsilon(rho)
+    real*8:: X(21), rho, T, rho_r, T_r
+    real*8, dimension(21, 7):: no, vo
+    real*8, dimension(3, 3):: ao, aoio
+    real*8, dimension(21):: rho_c, T_c
+    real*8:: eps = epsilon(X(1)), xi
     integer:: N = 21
 
     ao = 0
     do i = 1, N
         if (X(i) > eps) then
             call a_oio(rho, T, rho_c(i), T_c(i), no(i, :), vo(i, :), aoio)
-            ao(1, 1) = ao(1, 1) + X(i)*(aoio(1, 1) + log(X(i)))
-            ao(2, 1) = ao(2, 1) + X(i)/(rho_c(i)*rho_r)*aoio(2, 1)
-            ao(2, 2) = ao(2, 2) + X(i)*(T_c(i)/T_r)*aoio(2, 2)
-            ao(3, 1) = ao(3, 1) + X(i)*(1/(rho_c(i)/rho_r)**2)*aoio(3, 1)
-            ao(3, 2) = ao(3, 2) + X(i)*(T_c(i)/T_r)**2*aoio(3, 2)
-            ao(3, 3) = ao(3, 3) + X(i)*(T_c(i)/T_r)*(1/(rho_c(i)/rho_r))*aoio(3, 3)
+            xi = X(i)
+            ao(1, 1) = ao(1, 1) + xi*(aoio(1, 1) + log(xi))
+            ao(2, 1) = ao(2, 1) + xi*rho_r/rho_c(i)*aoio(2, 1)
+            ao(3, 1) = ao(3, 1) + xi*(rho_r/rho_c(i))**2*aoio(3, 1)
+            ! ao(3,3) should always be zero, based on the paper
+            ao(2, 2) = ao(2, 2) + xi*T_c(i)/T_r*aoio(2, 2)
+            ao(3, 2) = ao(3, 2) + xi*(T_c(i)/T_r)**2*aoio(3, 2)
         end if
     end do
 
@@ -315,16 +316,19 @@ Subroutine residual_term(X, delta, tau, &
                          Kpol, Kexp, nr, dr, tr, cr, &
                          Fij, Kpolij, Kexpij, dij, nij, tij, betaij, epsij, etaij, gammij, &
                          ar)
-    double precision, dimension(21):: X
-    integer, dimension(21):: Kpol, Kexp
-    double precision, dimension(21, 24):: nr, dr, tr, cr
-    double precision, dimension(21, 21, 12):: betaij, epsij, etaij, gammij
-    double precision, dimension(21, 21, 12):: dij, nij, tij
-    double precision, dimension(21, 21):: Fij
-    integer, dimension(21, 21):: Kpolij, Kexpij
-    double precision::delta, tau
-    double precision, dimension(3, 3):: ar, aoir, aijr
-    double precision:: eps = 0.0001
+    real*8, dimension(21), intent(in):: X
+    integer, dimension(21), intent(in):: Kpol, Kexp
+    real*8, dimension(21, 24), intent(in):: nr, tr
+    integer, dimension(21, 24), intent(in):: dr, cr
+    real*8, dimension(21, 21, 12), intent(in):: betaij, epsij, etaij, gammij
+    real*8, dimension(21, 21, 12), intent(in):: nij, tij
+    integer, dimension(21, 21, 12), intent(in):: dij
+    real*8, dimension(21, 21), intent(in):: Fij
+    integer, dimension(21, 21), intent(in):: Kpolij, Kexpij
+    real*8, intent(in):: delta, tau
+    real*8, dimension(3, 3), intent(inout):: ar
+    real*8, dimension(3, 3):: aoir, aijr
+    real*8:: eps = 0.0001
 
     integer:: i, j
 
@@ -332,8 +336,9 @@ Subroutine residual_term(X, delta, tau, &
     aoir = 0
     aijr = 0
 
-    do i = 1, size(X)
+    do i = 1, 21
         if (X(i) > eps) then
+            !write (0, *) "_Residual Term_", i, X(i)
             call a_oir(delta, tau, Kpol(i), Kexp(i), &
                        nr(i, :), dr(i, :), tr(i, :), cr(i, :), aoir)
             ar(1, 1) = ar(1, 1) + X(i)*aoir(1, 1)
@@ -342,12 +347,15 @@ Subroutine residual_term(X, delta, tau, &
             ar(3, 1) = ar(3, 1) + X(i)*aoir(3, 1)
             ar(3, 2) = ar(3, 2) + X(i)*aoir(3, 2)
             ar(3, 3) = ar(3, 3) + X(i)*aoir(3, 3)
+            !write (0, *) "ar_i21: ", aoir(2, 1)
         end if
     end do
 
-    do i = 1, size(X) - 1
-    do j = i + 1, size(X)
+    do i = 1, 20
+    do j = i + 1, 21
     if (Fij(i, j) > eps .and. X(i) > eps .and. X(j) > eps) then
+        !write (0, *) "_Departure Function_"
+        !write (0, *) i, j, Fij(i, j), Kpolij(i, j), Kexpij(i, j)
         call a_ijr(delta, tau, Kpolij(i, j), Kexpij(i, j), &
                    nij(i, j, :), dij(i, j, :), tij(i, j, :), etaij(i, j, :), &
                    epsij(i, j, :), gammij(i, j, :), betaij(i, j, :), &
@@ -361,6 +369,7 @@ Subroutine residual_term(X, delta, tau, &
     end if
     end do
     end do
+    !write (0, *) "ar21: ", ar(2, 1)
 
 End Subroutine residual_term
 
@@ -370,36 +379,42 @@ Program gerg
     Implicit None
     ! Ideal gas parameters
     character(len=100):: no_file, vo_file
-    double precision, dimension(21, 7):: no, vo
+    real*8, dimension(21, 7):: no, vo
     ! Residual energy parameteres
     character(len=100)::Kr_file, nr_file, dr_file, tr_file, cr_file
-    double precision, dimension(21, 24):: nr, dr, tr, cr
+    real*8, dimension(21, 24):: nr, tr
+    integer, dimension(21, 24):: cr, dr
     integer:: Kpol(21), Kexp(21)
     ! Departure function parameters
     character(len=100):: Fij_file, Kij_file, betaij_file, dij_file, epsij_file, &
                          etaij_file, gammij_file, nij_file, tij_file
     integer:: Kpolij(21, 21), Kexpij(21, 21)
-    double precision, dimension(21, 21, 12):: betaij, epsij, etaij, gammij
-    double precision, dimension(21, 21, 12):: dij, nij, tij
-    double precision, dimension(21, 21):: Fij
+    real*8, dimension(21, 21, 12):: betaij, epsij, etaij, gammij
+    real*8, dimension(21, 21, 12)::  nij, tij
+    integer, dimension(21, 21, 12)::  dij
+    real*8, dimension(21, 21):: Fij
     ! Reducing function parameters
     character(len=100):: reducing_file
-    double precision, dimension(21, 21):: Bv, Gv, Bt, Gt
+    real*8, dimension(21, 21):: Bv, Gv, Bt, Gt
     ! Critical properties parameters
     character(len=100):: critical_file
-    double precision, dimension(21):: rho_c, T_c, M
+    real*8, dimension(21):: rho_c, T_c, M
     ! Constants
-    double precision:: R
+    real*8:: R
     ! Variables
-    double precision:: T, rho, tau, delta, X(21)
+    real*8:: T, rho, tau, delta, X(21)
     ! Calculated variables
-    double precision, dimension(3, 3):: ar, ao
-    double precision:: P, Z, w, mean_M, rho2, old_rho, B, C, cp
-    double precision:: T_r, rho_r
+    real*8, dimension(3, 3):: ar, ao
+    real*8:: P, Z, w, cp, cv, mean_M
+    real*8:: T_r, rho_r
     character(len=100):: compound
-    integer:: i, j, io, stdin = 5, stdout = 6, stderr = 0
+
+    integer:: i, j, k, io, counter, stdin = 5, stdout = 6, stderr = 0
     character(len=100):: arg
 
+    real*8:: test_P, test_cv, test_cp, test_w
+
+    ! Files with all the parameters.
     ! Ideal Gas Parameters
     no_file = 'parameters/ideal/n'
     vo_file = 'parameters/ideal/v'
@@ -445,25 +460,34 @@ Program gerg
     T = 1.0
     X = 0
 
+    counter = 1
+
     select case (arg)
-    case ('isotherm')
-        ! Get Isothermic data of mixture at given T
-        read (stdin, *) T
+    case ('bintest')
+        open (1, file='GERG_test/binary/test_data')
         io = 0
         do while (io == 0)
-            read (stdin, *, iostat=io) i, compound, X(i)
-        end do
-        mean_M = 0
-        do i = 1, size(X)
-            mean_M = mean_M + X(i)*M(i)
-        end do
-        call reducing_funcs(X, Bv, Gv, Bt, Gt, rho_c, rho_r, T_c, T_r)
+            write (0, *) "___________________________________________________________"
+            X = 0
+            write (stderr, *) "Iteracion: ", counter
+            counter = counter + 1
 
-        tau = T_r/T
+            read (1, *, iostat=io) i, X(i), j, X(j), T, rho, test_P, test_cv, test_cp, test_w
 
-        do j = 1, 3000
-            rho = float(j)/100
+            if (io .ne. 0) then
+                write (0, *) "EOF"
+                exit
+            end if
+
+            mean_M = 0
+            do k = 1, size(X)
+                mean_M = mean_M + X(k)*M(k)
+            end do
+
+            call reducing_funcs(X, Bv, Gv, Bt, Gt, rho_c, rho_r, T_c, T_r)
+
             delta = rho*rho_r
+            tau = T_r/T
 
             call residual_term(X, delta, tau, &
                                Kpol, Kexp, nr, dr, tr, cr, &
@@ -475,18 +499,62 @@ Program gerg
 
             call zeta(delta, ar, Z)
 
+            call isobaric_heat(delta, tau, ao, ar, cp)
+
+            call sound_speed(R, T, mean_M/1000, delta, tau, ar, ao, w)
+
+            ! adim * [mol/L] * [K] * [J/(mol*K)] * [L/m3] * [MPa/Pa]
+            P = Z*rho*T*R*1000*1e-6
+            cp = cp*R
+            print *, i, j, T, rho, P, cv, cp, w, test_P, test_cv, test_cp, test_w
+
+        end do
+        close (1)
+
+    case ('isotherm')
+        open (1, file='debug_data')
+        ! Get Isothermic data of mixture at given T
+        read (1, *) T
+        io = 0
+        ! Get concentration data
+        do while (io == 0)
+            read (1, *, iostat=io) i, compound, X(i)
+            print *, i, X(i)
+        end do
+        close (1)
+        ! Calculate mean molecular weight
+        mean_M = 0
+        do i = 1, size(X)
+            mean_M = mean_M + X(i)*M(i)
+        end do
+        call reducing_funcs(X, Bv, Gv, Bt, Gt, rho_c, rho_r, T_c, T_r)
+
+        tau = T_r/T
+
+        do j = 0, 5000, 10
+            rho = float(j)/10
+            delta = rho*rho_r
+            tau = T_r/T
+
+            call residual_term(X, delta, tau, &
+                               Kpol, Kexp, nr, dr, tr, cr, &
+                               Fij, Kpolij, Kexpij, dij, nij, tij, betaij, &
+                               epsij, etaij, gammij, &
+                               ar)
+
+            call ideal_term(X, rho, T, rho_c, T_c, rho_r, T_r, &
+                            no, vo, &
+                            ao)
+
+            call zeta(delta, ar, Z)
+
             call sound_speed(R, T, mean_M/1000, delta, tau, ar, ao, w)
 
             ! adim * [mol/L] * [K] * [J/(mol*K)] * [L/m3]
-
             P = Z*rho*T*R*1000
+
             print *, T, rho, P/1e6, Z, w
         end do
     end select
-
-    write (stderr, *) mean_M, 1/rho_r, T_r
-    do i = 1, size(X)
-        write (stderr, *) X(i)
-    end do
 
 End Program gerg
--- modules.f90
+++ modules.f90
@@ -14,26 +14,28 @@ Contains
         Implicit None
         ! Ideal gas parameters
         character(len=100), intent(in):: no_file, vo_file
-        double precision, dimension(21, 7):: no, vo
+        real*8, dimension(21, 7):: no, vo
         ! Residual energy parameteres
         character(len=100), intent(in)::Kr_file, nr_file, dr_file, tr_file, cr_file
-        double precision, intent(inout), dimension(21, 24):: nr, dr, tr, cr
+        real*8, intent(inout), dimension(21, 24):: nr, tr
+        integer, intent(inout), dimension(21, 24):: cr, dr
         integer, intent(inout):: Kpol(21), Kexp(21)
         ! Departure function parameters
         character(len=100), intent(in):: Fij_file, Kij_file, betaij_file, dij_file, epsij_file, &
                                          etaij_file, gammij_file, nij_file, tij_file
         integer:: Kpolij(21, 21), Kexpij(21, 21)
-        double precision, dimension(21, 21, 12), intent(inout):: betaij, epsij, etaij, gammij
-        double precision, dimension(21, 21, 12), intent(inout):: dij, nij, tij
-        double precision, dimension(21, 21):: Fij
+        real*8, dimension(21, 21, 12), intent(inout):: betaij, epsij, etaij, gammij
+        real*8, dimension(21, 21, 12), intent(inout):: nij, tij
+        integer, intent(inout):: dij(21, 21, 12)
+        real*8, dimension(21, 21):: Fij
         ! Reducing functions parameters
         character(len=100), intent(in):: reducing_file
-        double precision, dimension(21, 21), intent(inout):: Bv, Gv, Bt, Gt
+        real*8, dimension(21, 21), intent(inout):: Bv, Gv, Bt, Gt
         ! Critical properties
         character(len=100):: critical_file
-        double precision, dimension(21), intent(inout):: rho_c, T_c, M
+        real*8, dimension(21), intent(inout):: rho_c, T_c, M
         ! Common parameters
-        integer:: i, j, k, compound, io, N = 21
+        integer:: i, j, k, io, N = 21
 
         no = 0
         vo = 0
@@ -60,6 +62,67 @@ Contains
         rho_c = 0
         T_c = 0
         M = 0
+
+        ! Get ideal gas parameters
+        ! ------------------------
+        open (1, file=no_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, (no(i, j), j=1, 7)
+        end do
+        close (1)
+
+        open (1, file=vo_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, (vo(i, j), j=4, 7)
+        end do
+        close (1)
+
+        ! Get Residual parameters
+        ! -----------------------
+        open (1, file=Kr_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, Kpol(i), Kexp(i)
+        end do
+        close (1)
+        open (1, file=nr_file)
+
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, (nr(i, j), j=1, Kpol(i) + Kexp(i))
+        end do
+        close (1)
+
+        open (1, file=dr_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, (dr(i, j), j=1, Kpol(i) + Kexp(i))
+        end do
+        close (1)
+
+        open (1, file=cr_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, (cr(i, j), j=Kpol(i) + 1, Kpol(i) + Kexp(i))
+        end do
+        close (1)
+
+        open (1, file=tr_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, (tr(i, j), j=1, Kpol(i) + Kexp(i))
+        end do
+
+        ! Get reducing function parameters
+        ! --------------------------------
+        open (1, file=reducing_file)
+        io = 0
+        do while (io == 0)
+            read (1, *, iostat=io) i, j, Bv(i, j), Gv(i, j), Bt(i, j), Gt(i, j)
+        end do
+        close (1)
 
         ! Get departure function parameters
         ! ---------------------------------
@@ -122,65 +185,6 @@ Contains
         end do
         close (1)
 
-        ! Get ideal gas parameters
-        ! ------------------------
-        open (1, file=no_file)
-        do i = 1, N
-            read (1, *) compound, (no(i, j), j=1, 7)
-        end do
-        close (1)
-
-        open (1, file=vo_file)
-        do i = 1, N
-            read (1, *) compound, (vo(i, j), j=4, 7)
-        end do
-        close (1)
-
-        ! Get Residual parameters
-        ! -----------------------
-        open (1, file=Kr_file)
-        io = 0
-        do while (io == 0)
-            read (1, *, iostat=io) compound, Kpol(compound), Kexp(compound)
-        end do
-        close (1)
-
-        open (1, file=nr_file)
-        io = 0
-        do while (io == 0)
-            read (1, *, iostat=io) i, (nr(i, j), j=1, Kpol(i) + Kexp(i))
-        end do
-        close (1)
-
-        open (1, file=dr_file)
-        io = 0
-        do while (io == 0)
-            read (1, *, iostat=io) i, (dr(i, j), j=1, Kpol(i) + Kexp(i))
-        end do
-        close (1)
-
-        open (1, file=cr_file)
-        io = 0
-        do while (io == 0)
-            read (1, *, iostat=io) i, (cr(i, j), j=Kpol(i) + 1, Kpol(i) + Kexp(i))
-        end do
-        close (1)
-
-        open (1, file=tr_file)
-        io = 0
-        do while (io == 0)
-            read (1, *, iostat=io) i, (tr(i, j), j=1, Kpol(i) + Kexp(i))
-        end do
-
-        ! Get reducing function parameters
-        ! --------------------------------
-        open (1, file=reducing_file)
-        io = 0
-        do while (io == 0)
-            read (1, *, iostat=io) i, j, Bv(i, j), Gv(i, j), Bt(i, j), Gt(i, j)
-        end do
-        close (1)
-
         ! Get critical properties
         ! -----------------------
         open (1, file=critical_file)
@@ -188,6 +192,8 @@ Contains
         do while (io == 0)
             read (1, *, iostat=io) i, rho_c(i), T_c(i), M(i)
         end do
+
+        !call robbed_ideal(no, vo)
     End Subroutine get_parameters
 End Module get_params
 
@@ -199,26 +205,36 @@ Module thermo_props  !
 
 Contains
     Subroutine zeta(delta, ar, z)
-        double precision:: delta, ar(3, 3)
-        double precision, intent(inout):: z
+        real*8, intent(in):: delta, ar(3, 3)
+        real*8, intent(inout):: z
         z = 1 + delta*ar(2, 1)
     End Subroutine zeta
 
     Subroutine isobaric_heat(delta, tau, ao, ar, cp)
-        double precision, intent(in):: delta, tau, ao(3, 3), ar(3, 3)
-        double precision, intent(inout) :: cp
-        cp = -tau**2*(ao(3, 2) + ar(3, 2)) &
-             + (1 + delta*ar(2, 1) - delta*tau*ar(3, 3))**2 &
-             /(1 + 2*delta*ar(2, 1) + delta**2*ar(3, 1))
+        real*8, intent(in):: delta, tau, ao(3, 3), ar(3, 3)
+        real*8, intent(inout) :: cp
+        real*8:: up, down
+
+        up = (1 + delta*Ar(2, 1) - delta*tau*ar(3, 3))**2
+        down = 1 + 2*delta*Ar(2, 1) + delta**2*Ar(3, 1)
+
+        cp = -tau**2*(Ao(3, 2) + Ar(3, 2)) + up/down
+
     End Subroutine isobaric_heat
 
     Subroutine sound_speed(R, T, M, delta, tau, Ar, Ao, w)
-        double precision, intent(in):: R, T, M, delta, tau, Ar(3, 3), Ao(3, 3)
-        double precision, intent(inout):: w
+        real*8, intent(in):: R, T, M, delta, tau, Ar(3, 3), Ao(3, 3)
+        real*8, intent(inout):: w
+        real*8:: up, down, td
+
+        td = tau*delta
+
+        up = (1 + delta*Ar(2, 1) - td*Ar(3, 3))**2
+        down = tau**2*(Ao(3, 2) + Ar(3, 2))
+
         w = 1 + 2*delta*Ar(2, 1) + delta**2*Ar(3, 1)
-        w = w - (1 + delta*Ar(2, 1) - delta*tau*Ar(3, 3))**2 &
-            /(tau**2*(Ao(3, 2) + Ar(3, 2)))
+        w = w - up/down
         w = sqrt(w*R*T/M)
+
     End Subroutine sound_speed
 End Module thermo_props
-
