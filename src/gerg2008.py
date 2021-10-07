1c2e803e6dcb1bd42d8859e0f2d6a4ab6726b7d9
Federico Benelli <fedebenelli@outlook.com>
2021-10-06 23:20:59 -0300

    normalize concentrations if needed

--- src/gerg2008.py
+++ src/gerg2008.py
@@ -3,13 +3,14 @@ from .fortran import gerg2008f
 from .fortran.gerg2008f import thermopropsf
 
 import numpy as np
+import warnings
 
 
 class GERG2008:
 
     name = "GERG2008"
 
-    self.valid_components = {
+    self.valid_components = [
         "methane",
         "nitrogen",
         "carbon_dioxide",
@@ -31,7 +32,7 @@ class GERG2008:
         "hydrogen_sulfide",
         "helium",
         "argon",
-    }
+    ]
 
     def validate_components(self, components):
         for component in components:
@@ -41,9 +42,73 @@ class GERG2008:
     def validate_ranges(self, temperature, pressure):
         pass
 
+    def set_concentration(self, composition):
+        methane = composition.get("methane", 0)
+        nitrogen = composition.get("nitrogen", 0)
+        carbon_dioxide = composition.get("carbon_dioxide", 0)
+        ethane = composition.get("ethane", 0)
+        propane = composition.get("propane", 0)
+        butane = composition.get("butane", 0)
+        isobutane = composition.get("isobutane", 0)
+        pentane = composition.get("pentane", 0)
+        isopentane = composition.get("isopentane", 0)
+        hexane = composition.get("hexane", 0)
+        heptane = composition.get("heptane", 0)
+        octane = composition.get("octane", 0)
+        nonane = composition.get("nonane", 0)
+        decane = composition.get("decane", 0)
+        hydrogen = composition.get("hydrogen", 0)
+        oxygen = composition.get("oxygen", 0)
+        carbon_monoxide = composition.get("carbon_monoxide", 0)
+        water = composition.get("water", 0)
+        hydrogen_sulfide = composition.get("hydrogen_sulfide", 0)
+        helium = composition.get("helium", 0)
+        argon = composition.get("argon", 0)
+
+        x = np.array(
+            [
+                methane,
+                nitrogen,
+                carbon_dioxide,
+                ethane,
+                propane,
+                butane,
+                isobutane,
+                pentane,
+                isopentane,
+                hexane,
+                heptane,
+                octane,
+                nonane,
+                decane,
+                hydrogen,
+                oxygen,
+                carbon_monoxide,
+                water,
+                hydrogen_sulfide,
+                helium,
+                argon,
+            ],
+            dtype="d",
+        )
+
+        sum_value = x.sum()
+
+        if 0.9999 < sum_value < 1.0001:
+            warnings.warn("Composition doesn't add '1', will be normalized")
+            x = x / sum_value
+
+        return x
+
     def calculate_properties(
         self, temperature, pressure, density, composition
     ):
+        r = 8.31
+
+        x = self.set_concentration(composition)
+
+        m = gerg2008f.mean_m(x)
+
         density_r, temperature_r = gerg2008f.reducing_funcs(x)
 
         delta = density * density_r
