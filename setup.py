8cc8bd40288af0ae07559ab92e8de6fe2f303136
fedebenelli <fedebenelli@outlook.com>
2021-10-14 14:55:05 -0300

    first setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
+
+gerg2008 = Extension(
+    name="pyforfluids.fortran.gerg2008f",
+    sources=[
+        "pyforfluids/fortran/parameters.f95",
+        "pyforfluids/fortran/gerg.f95",
+    ],
+)
+
+thermo_props = Extension(
+    name="pyforfluids.fortran.thermo_props",
+    sources=[
+        "pyforfluids/fortran/parameters.f95",
+        "pyforfluids/fortran/thermoprops.f95",
+    ],
+)
+
+if __name__ == "__main__":
+    setup(
+        name="pyforfluids",
+        description="Library for fluid thermodynamics calculations",
+        author="Federico Benelli",
+        author_email="federico.benelli@mi.unc.edu.ar",
+        packages=["pyforfluids", "pyforfluids.models", "pyforfluids.fortran"],
+        ext_modules=[gerg2008, thermo_props],
+    )
