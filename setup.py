2e11ef02cd42fcea48cf0941b6d1ca403b6210dd
fedebenelli <fedebenelli@outlook.com>
2021-10-28 21:25:06 -0300

    style

--- setup.py
+++ setup.py
@@ -1,33 +1,38 @@
-import setuptools  # noqa
+"""
+"""
+
+import os
+
 from numpy.distutils.core import Extension, setup
+
+rootdir = os.path.normpath(os.path.join(__file__, os.pardir))
 
 EXTENSIONS = [
     Extension(
         name="pyforfluids.fortran.gerg2008f",
         sources=[
-            "pyforfluids/fortran/parameters.f95",
-            "pyforfluids/fortran/gerg.f95",
+            os.path.join(rootdir, "pyforfluids/fortran/parameters.f95"),
+            os.path.join(rootdir, "pyforfluids/fortran/gerg.f95"),
         ],
     ),
     Extension(
         name="pyforfluids.fortran.thermo_props",
         sources=[
-            "pyforfluids/fortran/parameters.f95",
-            "pyforfluids/fortran/thermoprops.f95",
+            os.path.join(rootdir, "pyforfluids/fortran/parameters.f95"),
+            os.path.join(rootdir, "pyforfluids/fortran/thermoprops.f95"),
         ],
     ),
 ]
 
 PACKAGES = ["pyforfluids", "pyforfluids.models", "pyforfluids.fortran"]
 
-if __name__ == "__main__":
-    setup(
-        name="PyForFluids",
-        version="0.0.1",
-        description="Library for fluid thermodynamics calculations",
-        url="https://github.com/fedebenelli/pyforfluids",
-        author="Federico Benelli; María Candelaria Arpajou",
-        author_email="federico.benelli@mi.unc.edu.ar",
-        packages=PACKAGES,
-        ext_modules=EXTENSIONS,
-    )
+setup(
+    name="PyForFluids",
+    version="0.0.1",
+    description="Library for fluid thermodynamics calculations",
+    url="https://github.com/fedebenelli/pyforfluids",
+    author="Federico Benelli; María Candelaria Arpajou",
+    author_email="federico.benelli@mi.unc.edu.ar",
+    packages=PACKAGES,
+    ext_modules=EXTENSIONS,
+)
