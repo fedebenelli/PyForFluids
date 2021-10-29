c469ae164cf58a09e4254d606655099cf35b5368
Candelaria-Arpajou <69314665+Candelaria-Arpajou@users.noreply.github.com>
2021-10-29 14:56:34 -0300

    Corrección de test para validate_pt_values

--- tests/test_gerg.py
+++ tests/test_gerg.py
@@ -1,70 +1,12 @@
-import pytest
-
 import numpy as np
 
 import pyforfluids.models as models
 
-
-@pytest.fixture
-def model():
-    return models.GERG2008()
+import pytest
 
 
-# ------------ NORMAL
-@pytest.fixture
-def random_normal_temp():
-    return np.random.uniform(90.0, 450.0)
-
-
-@pytest.fixture
-def random_normal_press():
-    return np.random.uniform(0.0, 35.0)
-
-
-# ------------ EXTENDED
-@pytest.fixture
-def random_min_extended_temp():
-    return np.random.uniform(60.0, 90.0)
-
-
-@pytest.fixture
-def random_max_extended_temp():
-    return np.random.uniform(450.0, 700.0)
-
-
-@pytest.fixture
-def random_extended_press():
-    return np.random.uniform(35.0, 70.0)
-
-
-# ----------- INVALID
-@pytest.fixture
-def random_min_invalid_temp():
-    return np.random.uniform(00.0, 60.0)
-
-
-@pytest.fixture
-def random_max_invalid_temp():
-    return np.random.uniform(700.0, 1000.0)
-
-
-@pytest.fixture
-def random_invalid_press():
-    return np.random.uniform(70.0, 100.0)
-
-
-# ----------- NEGATIVE
-@pytest.fixture
-def random_negative_temp():
-    return -np.random.uniform(1.0, 100.0)
-
-
-@pytest.fixture
-def random_negative_press():
-    return -np.random.uniform(1.0, 100.0)
-
-
-def test_components(model):
+def test_components():
+    model = models.GERG2008()
     valid_components = model.valid_components
     model.validate_components(valid_components)
 
@@ -74,52 +16,58 @@ def test_components(model):
     print(e_info)
 
 
-def test_no_warnings(model, random_normal_press, random_normal_temp):
+def test_validate_pt_values_no_warnings():
+    random_normal_press = np.random.uniform(0.0, 35.0)
+    random_normal_temp = np.random.uniform(90.0, 450.0)
+    model = models.GERG2008()
     model.validate_pt_values(random_normal_press, random_normal_temp)
     assert True
 
 
-def test_with_warnings_1(
-    model, random_extended_press, random_min_extended_temp
-):
-    with pytest.warns(None) as w_info:
+def test_validate_pt_values_min_extended_range():
+    random_extended_press = np.random.uniform(35.1, 70.0)
+    min_random_extended_temp = np.random.uniform(60.0, 89.9)
+    model = models.GERG2008()
+    with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(
-            random_extended_press, random_min_extended_temp
+            random_extended_press, min_random_extended_temp
         )
     print(w_info)
 
 
-def test_with_warnings_2(
-    model, random_extended_press, random_max_extended_temp
-):
-    with pytest.warns(None) as w_info:
+def test_validate_pt_values_max_extended_range():
+    random_extended_press = np.random.uniform(35.1, 70.0)
+    max_random_extended_temp = np.random.uniform(450.1, 700.0)
+    model = models.GERG2008()
+    with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(
-            random_extended_press, random_max_extended_temp
+            random_extended_press, max_random_extended_temp
         )
     print(w_info)
 
 
-def test_with_warnings_3(
-    model, random_extended_press, random_min_invalid_temp
-):
-    with pytest.warns(None) as w_info:
-        model.validate_pt_values(
-            random_extended_press, random_min_invalid_temp
-        )
+def test_validate_pt_values_min_invalid_range():
+    random_invalid_press = np.random.uniform(70.1, 100.0)
+    min_random_invalid_temp = np.random.uniform(00.0, 59.9)
+    model = models.GERG2008()
+    with pytest.warns(UserWarning) as w_info:
+        model.validate_pt_values(random_invalid_press, min_random_invalid_temp)
     print(w_info)
 
 
-def test_with_warnings_4(
-    model, random_extended_press, random_max_invalid_temp
-):
-    with pytest.warns(None) as w_info:
-        model.validate_pt_values(
-            random_extended_press, random_max_invalid_temp
-        )
+def test_validate_pt_values_max_invalid_range():
+    random_invalid_press = np.random.uniform(70.1, 100.0)
+    max_random_invalid_temp = np.random.uniform(700.1, 1000.0)
+    model = models.GERG2008()
+    with pytest.warns(UserWarning) as w_info:
+        model.validate_pt_values(random_invalid_press, max_random_invalid_temp)
     print(w_info)
 
 
-def test_with_warnings_5(model, random_negative_press, random_negative_temp):
-    with pytest.warns(None) as w_info:
-        model.validate_pt_values(random_negative_press, random_negative_temp)
+def test_validate_pt_values_negative_range():
+    random_nagetive_press = -np.random.uniform(0.1, 100.0)
+    random_negative_temp = -np.random.uniform(0.1, 100.0)
+    model = models.GERG2008()
+    with pytest.warns(UserWarning) as w_info:
+        model.validate_pt_values(random_nagetive_press, random_negative_temp)
     print(w_info)
