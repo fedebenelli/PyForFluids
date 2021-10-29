89259d31167f82d6ebdece977452beea070bba72
Candelaria-Arpajou <69314665+Candelaria-Arpajou@users.noreply.github.com>
2021-10-29 15:08:30 -0300

    Agregado de espacios

--- tests/test_gerg.py
+++ tests/test_gerg.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+
 import pyforfluids.models as models
+
 
 import pytest
 
@@ -21,6 +23,7 @@ def test_validate_pt_values_no_warnings():
     random_normal_temp = np.random.uniform(90.0, 450.0)
     model = models.GERG2008()
     model.validate_pt_values(random_normal_press, random_normal_temp)
+
     assert True
 
 
@@ -28,6 +31,7 @@ def test_validate_pt_values_min_extended_range():
     random_extended_press = np.random.uniform(35.1, 70.0)
     min_random_extended_temp = np.random.uniform(60.0, 89.9)
     model = models.GERG2008()
+
     with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(
             random_extended_press, min_random_extended_temp
@@ -39,6 +43,7 @@ def test_validate_pt_values_max_extended_range():
     random_extended_press = np.random.uniform(35.1, 70.0)
     max_random_extended_temp = np.random.uniform(450.1, 700.0)
     model = models.GERG2008()
+
     with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(
             random_extended_press, max_random_extended_temp
@@ -50,6 +55,7 @@ def test_validate_pt_values_min_invalid_range():
     random_invalid_press = np.random.uniform(70.1, 100.0)
     min_random_invalid_temp = np.random.uniform(00.0, 59.9)
     model = models.GERG2008()
+
     with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(random_invalid_press, min_random_invalid_temp)
     print(w_info)
@@ -59,6 +65,7 @@ def test_validate_pt_values_max_invalid_range():
     random_invalid_press = np.random.uniform(70.1, 100.0)
     max_random_invalid_temp = np.random.uniform(700.1, 1000.0)
     model = models.GERG2008()
+
     with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(random_invalid_press, max_random_invalid_temp)
     print(w_info)
@@ -68,6 +75,7 @@ def test_validate_pt_values_negative_range():
     random_nagetive_press = -np.random.uniform(0.1, 100.0)
     random_negative_temp = -np.random.uniform(0.1, 100.0)
     model = models.GERG2008()
+
     with pytest.warns(UserWarning) as w_info:
         model.validate_pt_values(random_nagetive_press, random_negative_temp)
     print(w_info)
