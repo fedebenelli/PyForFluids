0f25d9bdadb0987bcdc7923d869a9e102a5c40aa
Federico Benelli <fedebenelli@outlook.com>
2021-11-11 19:55:45 -0300

    compositional derivatives for residual energy

--- pyforfluids/fortran/gerg.f95
+++ pyforfluids/fortran/gerg.f95
@@ -63,47 +63,47 @@ Subroutine reducing_funcs(X, rho_r, T_r, T_r_x, V_r_x)
    V_r_x = 0
 
    do i = 1, N
-           T_r_x(i) = 2.d0*X(i)*T_c(i)
-           V_r_x(i) = 2.d0*X(i)/(rho_(i))
+           T_r_x(i) = 2.d0 * X(i) * T_c(i)
+           V_r_x(i) = 2.d0 * X(i) / (rho_c(i))
 
-   do k = 1, i-1
+   do k = 1, i - 1
            vki = 1.d0 / 8.d0 * (rho_c(k) ** (- 1.d0 / 3.d0) + rho_c(i) ** (- 1.d0 / 3.d0)) ** 3
            tki = sqrt((T_c(k) * T_c(i)))
-           c_v = 2*Bv(k, i) * Gv(k, i) * vki
-           c_t = 2*Bt(k, i) * Gt(k, i) * tki
+           c_v = 2 * Bv(k, i) * Gv(k, i) * vki
+           c_t = 2 * Bt(k, i) * Gt(k, i) * tki
 
            x_sum = X(k) + X(i)
            x_pro = X(k) * X(i)
            xki = X(k) + X(i)
 
-           Bv_xki = Bv(k, i)**2*X(k) + X(i)
-           Bt_xki = Bt(k, i)**2*X(k) + X(i)
+           Bv_xki = Bv(k, i) ** 2 * X(k) + X(i)
+           Bt_xki = Bt(k, i) ** 2 * X(k) + X(i)
 
-           fv_ki = X(k)*x_sum/Bv_xki + x_pro/Bv_xki * (1.d0 - x_sum/Bv_xki)
-           ft_ki = X(k)*x_sum/Bt_xki + x_pro/Bt_xki * (1.d0 - x_sum/Bt_xki)
+           fv_ki = X(k) * x_sum / Bv_xki + x_pro / Bv_xki * (1.d0 - x_sum / Bv_xki)
+           ft_ki = X(k) * x_sum / Bt_xki + x_pro / Bt_xki * (1.d0 - x_sum / Bt_xki)
 
-           V_r_x(i) = T_r_x(i) + c_v*fv_ki
-           T_r_x(i) = V_r_x(i) + c_t*ft_ki
+           V_r_x(i) = T_r_x(i) + c_v * fv_ki
+           T_r_x(i) = V_r_x(i) + c_t * ft_ki
            
    end do
-   do k = i+1, N
+   do k = i + 1, N
            vik = 1.d0 / 8.d0 * (rho_c(i) ** (- 1.d0 / 3.d0) + rho_c(k) ** (- 1.d0 / 3.d0)) ** 3
            tik = sqrt((T_c(i) * T_c(k)))
-           c_v = 2*Bv(i, k) * Gv(i, k) * vik
-           c_t = 2*Bt(i, k) * Gt(i, k) * tik
+           c_v = 2 * Bv(i, k) * Gv(i, k) * vik
+           c_t = 2 * Bt(i, k) * Gt(i, k) * tik
 
            x_sum = X(i) + X(k)
            x_pro = X(i) * X(k)
            xki = X(i) + X(k)
 
-           Bv_xik = Bv(i, k)**2*X(i) + X(k)
-           Bt_xik = Bt(i, k)**2*X(i) + X(k)
+           Bv_xik = Bv(i, k) ** 2 * X(i) + X(k)
+           Bt_xik = Bt(i, k) ** 2 * X(i) + X(k)
 
-           fv_ik = X(i)*x_sum/Bv_xik + x_pro/Bv_xik * (1.d0 - x_sum/Bv_xik)
-           ft_ik = X(i)*x_sum/Bt_xik + x_pro/Bt_xik * (1.d0 - x_sum/Bt_xik)
+           fv_ik = X(i) * x_sum / Bv_xik + x_pro / Bv_xik * (1.d0 - x_sum / Bv_xik)
+           ft_ik = X(i) * x_sum / Bt_xik + x_pro / Bt_xik * (1.d0 - x_sum / Bt_xik)
 
-           V_r_x(i) = T_r_x(i) + c_v*fv_ik
-           T_r_x(i) = V_r_x(i) + c_t*ft_ik
+           V_r_x(i) = T_r_x(i) + c_v * fv_ik
+           T_r_x(i) = V_r_x(i) + c_t * ft_ik
    end do
    end do
 
@@ -378,33 +378,68 @@ Subroutine ideal_term(X, rho, T, rho_r, T_r, ao)
    end do
 End Subroutine ideal_term
 
-Subroutine residual_term(X, delta, tau, ar)
+Subroutine residual_term(X, delta, tau, ar, ar_x, ar_dx, ar_tx, ar_xx)
    use parameters
    real(8), intent(in) :: delta, tau, X(21)
    real(8), dimension(3, 3), intent(out) :: ar
+   real(8), dimension(21), intent(out) :: ar_x, ar_dx, ar_tx, ar_xx
    real(8), dimension(3, 3) :: aoir, aijr
-   integer :: i, j
+   real(8) :: F
+   integer :: i, j, temp_i, temp_k
 
    call get_params()
 
-   ar = 0.0
+   ar = 0
+   ar_x = 0
+   ar_dx = 0
+   ar_tx = 0
+   ar_xx = 0
 
    do i = 1, N
       if (X(i) > eps) then
          aoir = 0.0
-         aijr = 0.0
+
          call a_oir(delta, tau, Kpol(i), Kexp(i), &
                     noik(i, :), doik(i, :), toik(i, :), coik(i, :), aoir)
+
          ar(1, 1) = ar(1, 1) + X(i) * aoir(1, 1)
          ar(2, 1) = ar(2, 1) + X(i) * aoir(2, 1)
          ar(2, 2) = ar(2, 2) + X(i) * aoir(2, 2)
          ar(3, 1) = ar(3, 1) + X(i) * aoir(3, 1)
          ar(3, 2) = ar(3, 2) + X(i) * aoir(3, 2)
          ar(3, 3) = ar(3, 3) + X(i) * aoir(3, 3)
-      end if
-   end do
+      
+         ar_x(i) = ar(1, 1)
+         ar_dx(i) = ar(2, 1)
+         ar_tx(i) = ar(2, 2)
+         ar_xx(i) = 0
 
-   ! write (0, *) "Normal:", ar(2, 1), ar(3, 1), ar(2, 2), ar(3, 2), ar(3, 3)
+         do k = 1, N
+         temp_i = i
+         temp_k = k
+         F = Fij(temp_i, temp_k)
+         if (F < eps .and. Fij(k, i) > eps) then
+                 ! Invert variables for cases like i=2, k=1
+                 ! TODO: NOT SURE IF THIS SHOULD BE DONE!
+                 temp_i = k
+                 temp_k = i
+                 F = Fij(temp_i, temp_k)
+         end if
+         if (k /= i .and. X(k) > eps .and. F > eps) then
+         aijr = 0.0
+         call a_ijr(delta, tau, Kpolij(temp_i, temp_k), Kexpij(i, temp_k), &
+                 nij(temp_i, temp_k, :), dij(temp_i, temp_k, :),&
+                 tij(temp_i, temp_k, :), ethaij(temp_i, temp_k, :), &
+                 epsij(temp_i, temp_k, :), gammaij(temp_i, temp_k, :),&
+                 betaij(temp_i, temp_k, :), aijr)
+
+         ar_x(i) = ar_x(i) + X(k) * Fij(i, k) * aijr(1, 1)
+         ar_dx(i) = ar_dx(i) + X(k) * Fij(i, k) * aijr(2, 1)
+         ar_tx(i) = ar_tx(i) + X(k) * Fij(i, k) * aijr(2, 2)
+         end if
+         end do
+   end if
+   end do
 
    do i = 1, N - 1
    do j = i + 1, N
@@ -422,7 +457,13 @@ Subroutine residual_term(X, delta, tau, ar)
    end if
    end do
    end do
+
+   
 End Subroutine residual_term
+
+program main
+        print *, "this only exists to compile with gfortran"
+end program main
 
 ! Program gerg
 ! use parameters
