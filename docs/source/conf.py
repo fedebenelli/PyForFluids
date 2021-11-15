0973c869c81a830e61104ff65e9aadd216a34140
Federico Benelli <fedebenelli@outlook.com>
2021-11-15 20:30:51 -0300

    mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
 
-MOCK_MODULES = ['fortran']
+MOCK_MODULES = ['fortran', 'pyforfluids.fortran']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
