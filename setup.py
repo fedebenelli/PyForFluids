1556d15c19ca1c5a690380b6a320156f9f6a3c69
Federico Benelli <fedebenelli@outlook.com>
2021-11-15 17:02:32 -0300

    hope this works on RTD

--- setup.py
+++ setup.py
@@ -5,29 +5,33 @@ import os
 
 from numpy.distutils.core import Extension, setup
 
-rootdir = os.path.normpath(os.path.join(__file__, os.pardir))
-fortrandir = os.path.join(rootdir, "pyforfluids", "fortran")
-
+ROOT_DIR = os.path.normpath(os.path.join(__file__, os.pardir))
+FORTRAN_DIR = os.path.join(ROOT_DIR, "pyforfluids", "fortran")
 REQUIREMENTS = ["numpy>=1.21.2"]
 
 EXTENSIONS = [
     Extension(
         name="pyforfluids.fortran.gerg2008f",
         sources=[
-            os.path.join(fortrandir, "parameters.f95"),
-            os.path.join(fortrandir, "gerg.f95"),
+            os.path.join(FORTRAN_DIR, "parameters.f95"),
+            os.path.join(FORTRAN_DIR, "gerg.f95"),
         ],
     ),
     Extension(
         name="pyforfluids.fortran.thermo_props",
         sources=[
-            os.path.join(fortrandir, "parameters.f95"),
-            os.path.join(fortrandir, "thermoprops.f95"),
+            os.path.join(FORTRAN_DIR, "parameters.f95"),
+            os.path.join(FORTRAN_DIR, "thermoprops.f95"),
         ],
     ),
 ]
 
 PACKAGES = ["pyforfluids", "pyforfluids.models", "pyforfluids.fortran"]
+
+ON_RTD = os.environ.get("READTHEDOCS", None) == "True"
+
+if ON_RTD:
+    EXENSIONS = []
 
 setup(
     name="PyForFluids",
