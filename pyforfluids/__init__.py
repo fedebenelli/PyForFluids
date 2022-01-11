0b649c85bea6b0a1f3c812fa3bdbd64a2d64e15f
fedebenelli <fedebenelli@outlook.com>
2022-01-11 11:57:09 -0300

    version

--- pyforfluids/__init__.py
+++ pyforfluids/__init__.py
@@ -2,6 +2,9 @@
 
 Fluid properties simulation based on Ecuations of State.
 """
+
+__version__ = "0.0.1a2"
+
 from .core import Fluid  # noqa
 from . import fortran  # noqa
 from . import models  # noqa
