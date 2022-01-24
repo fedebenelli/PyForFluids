027b27127f2a5e01d19b6514a2c69cd9a1beb291
fedebenelli <fedebenelli@outlook.com>
2022-01-24 15:52:23 -0300

    trying to fix mol derivs

--- pyforfluids/fortran/thermoprops.f95
+++ pyforfluids/fortran/thermoprops.f95
@@ -337,11 +337,11 @@ Contains
       real(8), intent(out) :: JT
       real(8) :: up, down_1, down_2
 
-      up = -(delta* Ar(2,1) + delta**2*Ar(3,1) + delta*tau*Ar(3,3))
-      down_1 = (1.d0 + delta * Ar(2,1) - delta * tau * Ar (3,3))**2
-      down_2 = -tau**2 *(Ao(3,2) + Ar(3,2))*(1.d0 + 2.d0*delta*Ar(2,1)+delta**2 * Ar(3,1))
+      up = - (delta * Ar(2, 1) + delta ** 2 * Ar(3, 1) + delta * tau * Ar(3, 3))
+      down_1 = (1.d0 + delta * Ar(2, 1) - delta * tau * Ar (3, 3)) ** 2
+      down_2 = - tau ** 2 * (Ao(3, 2) + Ar(3, 2)) * (1.d0 + 2.d0 * delta * Ar(2, 1) + delta ** 2 * Ar(3, 1))
 
-      JT = up/(down_1 + down_2) /(R * rho*1000)
+      JT = up / (down_1 + down_2) / (R * rho * 1000)
 
    End Subroutine joule_thomson_coeff
 
@@ -376,35 +376,25 @@ Contains
       C = Ar(3, 1) / (rho_r ** 2)
    End Subroutine third_thermal_virial_coeff
 
-   Subroutine helmholtz_per_mol(x, delta, tau, rho_r, T_r, ar, ar_x, ar_dx, &
-                                v_r_x, T_r_x, dar_dn, dadr_dn)
+   Subroutine helmholtz_per_mol(x, delta, tau, rho_r, t_r, ar, ar_x, ar_dx, &
+                                dvr_dx, dtr_dx, dar_dn, dadr_dn)
+           Implicit None
            real(8), intent(in) :: x(21), delta, tau, rho_r, T_r, &
-                   ar(3,3), ar_x(21), ar_dx(21), v_r_x(21), T_r_x(21)
+                   ar(3, 3), ar_x(21), ar_dx(21), dvr_dx(21), dtr_dx(21)
            real(8), dimension(21), intent(out) :: dar_dn, dadr_dn
-           real(8), dimension(21) :: drhor_dn, dtr_dn
-           integer :: i
+           real(8), dimension(21) :: drhor_dx, drhor_dn, dtr_dn
 
-           dar_dn = 0.d0
-           dadr_dn = 0.d0
+           drhor_dx = - rho_r ** 2 * dvr_dx
 
-           drhor_dn = -rho_r**2*v_r_x
-           dtr_dn = v_r_x
+           drhor_dn = drhor_dx - sum(x * drhor_dx)
+           dtr_dn = dtr_dx - sum(x * dtr_dx)
 
-           do i=1, size(dtr_dn)
-                drhor_dn = drhor_dn - (-x(i)*(-rho_r**2*v_r_x(i)))
-                dtr_dn = dtr_dn - x(i)*t_r_x(i)
-           end do
+           dar_dn = delta * ar(2, 1) * (1.d0 - drhor_dn / rho_r) &
+                  + tau * ar(2, 2) / t_r * dtr_dn + ar_x - sum(x * ar_x)
 
-           dar_dn = delta*ar(2, 1)*(1.d0 - 1.d0/rho_r * dtr_dn)
-           dar_dn = dar_dn + tau*ar(2, 2)/T_r * dtr_dn + ar_x
+           dadr_dn = delta * ar(3, 1) * (1.d0 - drhor_dn / rho_r) &
+                   + tau * ar(3, 3) / t_r * dtr_dn + ar_dx - sum(x * ar_dx)
 
-           dadr_dn = delta*ar(3, 1)*(1.d0 - 1.d0/rho_r * dtr_dn)
-           dadr_dn = dadr_dn + tau*ar(3, 3)/T_r * dtr_dn + ar_dx
-
-           do i = 1, size(dtr_dn)
-                   dar_dn = dar_dn - X(i)*ar_x(i)
-                   dadr_dn = dadr_dn - X(i)*ar_dx(i)
-           end do
    End Subroutine helmholtz_per_mol
 
 End Module thermo_props
