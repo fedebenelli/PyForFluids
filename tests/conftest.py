a72da6fb5d4acf9eefb4189ac61f2a1b3d7b3699
Federico E. Benelli <fedebenelli@outlook.com>
2022-01-25 17:35:13 -0300

    synced with main

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import os
 import pathlib
 import pickle as pkl
 
+import pandas as pd
+
 import pytest
 
 PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
@@ -18,3 +20,11 @@ def isotherm(data_path):
     path = data_path("isotherm.pkl")
     with open(path, "rb") as f:
         return pkl.load(f)
+
+
+@pytest.fixture()
+def ahmadi323(data_path):
+    path = data_path("ahmadi323.3.csv")
+    df = pd.read_csv(path)
+    df = df.drop("temperature", axis=1)
+    return df
--- tests/test_gerg.py
+++ tests/test_gerg.py
@@ -1,17 +1,91 @@
+import numpy as np
+
 import pyforfluids.models as models
 
 import pytest
 
 
-@pytest.fixture
-def model():
-    return models.GERG2008()
+# -- Components related tests -------------------------------------------------
 
 
-def test_components(model):
+def test_components():
+    model = models.GERG2008()
     valid_components = model.valid_components
     model.validate_components(valid_components)
 
     with pytest.raises(Exception):
         wrong_component = "other_string"
         model.validate_components(valid_components + [wrong_component])
+
+
+def test_normalizer():
+    model = models.GERG2008()
+    composition = {"methane": 0.5, "ethane": 2}
+
+    with pytest.warns(UserWarning):
+        model.set_concentration(composition)
+
+
+# -- Valid ranges tests -------------------------------------------------------
+
+
+@pytest.mark.skip(reason="no way of currently testing this")
+def test_validate_pt_values_no_warnings():
+    random = np.random.default_rng(seed=42)
+    random_normal_press = random.uniform(0.0, 35.0e6)
+    random_normal_temp = random.uniform(90.0, 450.0)
+    model = models.GERG2008()
+    model.validate_ranges(random_normal_temp, random_normal_press)
+
+
+def test_validate_pt_values_min_extended_range():
+    random = np.random.default_rng(seed=42)
+    random_extended_press = random.uniform(35.1e6, 70.0e6)
+    min_random_extended_temp = random.uniform(60.0, 89.9)
+    model = models.GERG2008()
+
+    with pytest.warns(UserWarning):
+        model.validate_ranges(min_random_extended_temp, random_extended_press)
+
+
+def test_validate_pt_values_max_extended_range():
+    random = np.random.default_rng(seed=42)
+    random_extended_press = random.uniform(35.1e6, 70.0e6)
+    max_random_extended_temp = random.uniform(450.1, 700.0)
+    model = models.GERG2008()
+
+    with pytest.warns(UserWarning):
+        model.validate_ranges(max_random_extended_temp, random_extended_press)
+
+
+def test_validate_pt_values_min_invalid_range():
+    random = np.random.default_rng(seed=42)
+    random_invalid_press = random.uniform(70.1e6, 100.0e6)
+    min_random_invalid_temp = random.uniform(00.0, 59.9)
+    model = models.GERG2008()
+
+    with pytest.warns(UserWarning):
+        model.validate_ranges(min_random_invalid_temp, random_invalid_press)
+
+
+def test_validate_pt_values_max_invalid_range():
+    random = np.random.default_rng(seed=42)
+    random_invalid_press = random.uniform(70.1e6, 100.0e6)
+    max_random_invalid_temp = random.uniform(700.1, 1000.0)
+    model = models.GERG2008()
+
+    with pytest.warns(UserWarning):
+        model.validate_ranges(max_random_invalid_temp, random_invalid_press)
+
+
+def test_validate_pt_values_negative_range():
+    random = np.random.default_rng(seed=42)
+    random_nagetive_press = -random.uniform(0.1e6, 100.0e6)
+    random_negative_temp = -random.uniform(0.1, 100.0)
+    model = models.GERG2008()
+
+    with pytest.warns(UserWarning):
+        model.validate_ranges(random_negative_temp, random_nagetive_press)
+
+
+# -----------------------------------------------------------------------------
