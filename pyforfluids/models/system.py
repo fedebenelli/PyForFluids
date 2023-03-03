569a79f43a7f9301a20cf105047a7b8db4614693
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2023-03-03 17:19:39 -0300

    feat(system): general gradient and hess functions,  and thermo methods
    for readability added two functions that construct the jitted first and second derivatives, now all
    the helmholtz functions are included in the init. added a pressure calculation method and volume
    solver that uses scipy. Both need to be extended still

--- pyforfluids/models/system.py
+++ pyforfluids/models/system.py
@@ -1,32 +1,81 @@
 """
 """
-from jax import jacrev, jit
+from jax import grad, jacrev, jit
+
+from pyforfluids.constants import R
+
+from scipy.optimize import root_scalar
+
+
+def get_grad(fun, nvar):
+    return jit(grad(fun, argnums=nvar))
+
+
+def get_hess(fun, nvar_1, nvar_2):
+    return jit(jacrev(jit(grad(fun, argnums=nvar_1)), argnums=nvar_2))
 
 
 class System:
-    def __init__(self, armodel, gemodel, idealmodel):
+    def __init__(self, armodel=None, gemodel=None, idealmodel=None):
         self.armodel = armodel
         self.gemodel = gemodel
         self.idealmodel = idealmodel
 
-        if armodel.autodiff:
-            self._ar = jit(armodel.residual_helmholtz)
-            self._dar = jit(
-                jacrev(armodel.residual_helmholtz, argnums=[0, 1, 2])
-            )
+        if armodel:
+            self.__setup_armodel_functions(armodel)
 
-    def residual_helmholtz(self, z, volume, temperature):
-        ar = self._ar(z, volume, temperature)
-        dar = self._dar(z, volume, temperature)
-        
-        return ar, dar
+    def __setup_armodel_functions(self, armodel):
+        if armodel.autodiff:
+            # Extract the Ar function
+            ar_fun = armodel.residual_helmholtz
+
+            # Function valuation
+            self.ar = jit(ar_fun)
+
+            # Only compositional derivatives
+            self.dar_dn = get_grad(ar_fun, 0)
+
+            # Only volume derivative
+            self.dar_dv = get_grad(ar_fun, 1)
+
+            # Only temperature derivative
+            self.dar_dt = get_grad(ar_fun, 2)
+
+            # Second derivatives
+            self.dar2_dn2 = get_hess(ar_fun, 0, 0)
+            self.dar2_dtn = get_hess(ar_fun, 1, 0)
+            self.dar2_dvn = get_hess(ar_fun, 2, 0)
+
+            self.dar2_dt2 = get_hess(ar_fun, 1, 1)
+            self.dar2_dtv = get_hess(ar_fun, 2, 1)
+            self.dar2_dv2 = get_hess(ar_fun, 2, 2)
 
     def ln_phi(self, z, volume, temperature):
-        # residual_helmholtz = self.residual_helmholtz(z, volume, temperature)
         ...
 
     def pressure(self, z, volume, temperature):
-        ...
+        dar_dv = self.dar_dv(z, volume, temperature)
+        pressure = -dar_dv + z.sum() * R * temperature / volume
+        return pressure
 
     def volume(self, z, pressure, temperature):
-        ...
+        def obj_pressure(volume):
+            dar_dv = self.dar_dv(z, volume, temperature)
+            dar2_dv2 = self.dar2_dv2(z, volume, temperature)
+
+            rt = R * temperature
+            rt_v = rt / volume
+            n = z.sum()
+
+            p = -dar_dv + n * rt_v
+            dp = -rt * dar2_dv2 - n * rt_v / volume
+
+            return p - pressure, dp
+
+        v0 = R * temperature / pressure
+
+        volume = root_scalar(
+            obj_pressure, method="newton", fprime=True, x0=v0, xtol=1e-5
+        ).root
+
+        return volume
