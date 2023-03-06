042312c21572258862290a3fdfe5f149a5de4ced
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2023-03-06 11:34:35 -0300

    feat(mixingrules): Full mix instead of individual methods to check performance differences

--- pyforfluids/models/mixingrules/mixingrules.py
+++ pyforfluids/models/mixingrules/mixingrules.py
@@ -12,8 +12,38 @@ class ClassicVdW:
         self._lij = lij
 
     @partial(jit, static_argnames=["self"])
+    def mix(self, z, v, t, a, b, c, del1, del2):
+        n = len(z)
+        amix = 0
+        bmix = 0
+        cmix = 0
+        del1mix = 0
+        del2mix = 0
+
+        for i in range(n):
+            for j in range(i, n):
+                amix += (
+                    2
+                    * z[i]
+                    * z[j]
+                    * np.sqrt(a[i] * a[j])
+                    * (1 - self._kij[i, j])
+                )
+
+                # Not dividing by two since it's multiplied by two implicitly
+                bmix += z[i] * z[j] * (b[i] + b[j]) * (1 - self._lij[i, j])
+
+            cmix += z[i] * c[i]
+
+        del1mix = del1[0]
+        del2mix = del1[0]
+        bmix = bmix / np.sum(z)
+
+        return amix, bmix, cmix, del1mix, del2mix
+
+    @partial(jit, static_argnames=["self"])
     def mix_a(self, z, v, t, a):
-        n = len(a)
+        n = len(z)
         amix = 0
 
         for i in range(n):
