f82fda5697ab37323ff380481d9bfd57b3cf7913
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2023-03-08 14:22:58 -0300

    feat(pr76): convert to jax arrays on init

--- pyforfluids/models/cubic/peng_robinson_76.py
+++ pyforfluids/models/cubic/peng_robinson_76.py
@@ -5,16 +5,24 @@ from jax import jit
 
 from pyforfluids.constants import R
 
+from pyforfluids.models.cubic.cubic_eos import CubicEoS
 
-class PR76:
-    def __init__(self, tc, pc, w):
+
+class PR76(CubicEoS):
+    """
+    Peng Robinson 76 Equation of State
+    """
+
+    def __init__(self, tc, pc, w, mixing_rule):
+        self.set_mixingrule(mixing_rule)
+        self._del1 = np.array([1 + np.sqrt(2.0)])
+        self._del2 = np.array([1 - np.sqrt(2.0)])
+
+        self._tc = np.array(tc)
+        self._pc = np.array(pc)
+        self._w = np.array(w)
+
         self.n = len(tc)
-        self._del1 = [1 + np.sqrt(2.0)]
-        self._del2 = [1 - np.sqrt(2.0)]
-
-        self._tc = tc
-        self._pc = pc
-        self._w = w
 
         self._ac = 0.45723553 * R**2 * tc**2 / pc
         self._b = 0.07779607 * R * tc / pc
@@ -35,9 +43,9 @@ class PR76:
         return self._c
 
     @partial(jit, static_argnames=["self"])
-    def del1(self, z, v, t):
+    def delta1_parameter(self, z, v, t):
         return self._del1
 
     @partial(jit, static_argnames=["self"])
-    def del2(self, z, v, t):
+    def delta2_parameter(self, z, v, t):
         return self._del2
