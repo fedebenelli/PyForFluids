3e79513c338fe9135bf125e4ab9edea96583c912
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2023-03-08 14:18:51 -0300

    feat(models): abstract objects for models and mixing rules for Cubic EoS

--- pyforfluids/models/core_models.py
+++ pyforfluids/models/core_models.py
@@ -1,22 +1,47 @@
 """
+Abstract objects used to define the basic skeletons needed.
 """
 
 from abc import ABCMeta, abstractmethod
 
 
-class ArModel(ABCMeta):
-    """ """
+class ArModel(metaclass=ABCMeta):
+    """Residual Helmholtz Model"""
 
     @abstractmethod
-    def residual_helmholtz(cls, z, volume, temperature):
+    def residual_helmholtz(self, z, volume, temperature):
         """Method to calculate residual helmholtz"""
         raise NotImplementedError
 
 
-class GeModel(ABCMeta):
-    """ """
+class GeModel(metaclass=ABCMeta):
+    """Excess Gibbs Model"""
 
     @abstractmethod
-    def excess_gibbs(cls, z, volume, temperature):
+    def excess_gibbs(self, z, volume, temperature):
         """Method to calculate excess Gibbs energy"""
+        raise NotImplementedError
+
+
+class CubicMixingRule(metaclass=ABCMeta):
+    """Mixing Rule to use inside a Cubic Equation of State."""
+
+    @abstractmethod
+    def mix_a(self, z, v, t, a):
+        raise NotImplementedError
+
+    @abstractmethod
+    def mix_b(self, z, v, t, b):
+        raise NotImplementedError
+
+    @abstractmethod
+    def mix_c(self, z, v, t, c):
+        raise NotImplementedError
+
+    @abstractmethod
+    def mix_delta1(self, z, v, t, delta1):
+        raise NotImplementedError
+
+    @abstractmethod
+    def mix_delta2(self, z, v, t, delta2):
         raise NotImplementedError
