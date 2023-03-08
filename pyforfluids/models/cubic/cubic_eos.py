0334aa8ee3f22121649c1ffa3e4b59c20ca0768c
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2023-03-08 14:22:29 -0300

    refactor(cubiceos): initial volume and parameters
    Now the CubicEoS class has a method to give a volume initializer for the volume solver method. And
    all the parameters calculations are now part of this class, only the mixing rule is added to it

--- pyforfluids/models/cubic/cubic_eos.py
+++ pyforfluids/models/cubic/cubic_eos.py
@@ -1,55 +1,101 @@
 """
 """
+from abc import abstractmethod
 from functools import partial
 
 import jax.numpy as np
 from jax import jit
 
 from pyforfluids.constants import R
-from pyforfluids.models.core_models import ArModel
+from pyforfluids.models.core_models import ArModel, CubicMixingRule
 
 
-class CubicEoS(metaclass=ArModel):
-    """Generic CubicEoS"""
+class CubicEoS(ArModel):
+    """Generic CubicEoS.
+
+    Base class that contains the logic to calculate the residual Helmhotlz
+    energy with a generic Cubic Equation of State.
+    """
 
     autodiff = True
 
-    def __init__(self, equation, mixing_rule):
-        self.equation = equation
-        self.mixing_rule = mixing_rule
+    def set_mixingrule(self, mixingrule: CubicMixingRule):
+        self.mixing_rule = mixingrule
 
     def initial_volume(self, z, pressure, temperature, root="gas"):
+        """Initial volume specifier.
+
+        Determine the initial volume to use on a volume solver, based the
+        desired root to obtain.
+
+        Parameters
+        ----------
+        z: array
+            Concentrations vector
+        pressure: float
+            Pressure where to calculate the volume
+        temperature:
+            Temperature where to calculate the volume
+        root: string
+            Desired root to obtain, defaults to gas
+        """
         if root == "gas":
             return R * temperature / pressure
         elif root == "liquid":
-            b = self.equation.repulsive_parameter(z, 1, temperature)
-            v0 = self.mixing_rule.mix_b(z, 1, temperature, b) * 0.9
+            b = self.repulsive_parameter(z, 1, temperature)
+            v0 = self.mixing_rule.mix_b(z, 1, temperature, b)
             return v0
 
     @partial(jit, static_argnames=["self"])
-    def residual_helmholtz(self, z, v, t):
-        apures = self.equation.attractive_parameter(z, v, t)
-        bpures = self.equation.repulsive_parameter(z, v, t)
-        # cpures = self.equation.volume_shift(z, v, t)
+    def residual_helmholtz(self, z, volume, temperature):
+        """Residual Helmholtz method.
 
+        Calculate the residual Helmholtz energy based on the generic cubic
+        equation of state:
+        """
+        v = volume
+        t = temperature
+
+        # Pure compounds parameters
+        apures = self.attractive_parameter(z, v, t)
+        bpures = self.repulsive_parameter(z, v, t)
+        delta1 = self.delta1_parameter(z, v, t)
+        delta2 = self.delta2_parameter(z, v, t)
+
+        # Mixture "as a fluid" parameters
         a = self.mixing_rule.mix_a(z, v, t, apures)
         b = self.mixing_rule.mix_b(z, v, t, bpures)
-        # c = self.mixing_rule.mix_c(z, v, t, cpures)
+        del1 = self.mixing_rule.mix_delta1(z, v, t, delta1)
+        del2 = self.mixing_rule.mix_delta2(z, v, t, delta2)
 
-        del1 = self.equation._del1[0]
-        del2 = self.equation._del2[0]
+        n = z.sum()
+        b_v = b / v
 
         residual_helmholtz = (
-            (
-                -np.sum(z) * np.log(1.0 - b / v)
-                - a
-                / (R * t * b)
-                * 1.0
-                / (del1 - del2)
-                * np.log((1 + del1 * b / v) / (1 + del2 * b / v))
-            )
-            * R
-            * t
+            -np.log(1 - b_v)
+            - a/(n*R*t*b) * 1 / (del1 - del2)
+            * np.log((1 + del1*b_v)/(1 + del2*b_v))
         )
+        # R*t
 
         return residual_helmholtz
+
+    @abstractmethod
+    def attractive_parameter(self, z, v, t):
+        raise NotImplementedError
+
+    @abstractmethod
+    def repulsive_parameter(self, z, v, t):
+        raise NotImplementedError
+
+    @abstractmethod
+    def delta1_parameter(self, z, v, t):
+        raise NotImplementedError
+
+    @abstractmethod
+    def delta2_parameter(self, z, v, t):
+        raise NotImplementedError
+
+    @abstractmethod
+    def volume_shift(self, z, v, t):
+        raise NotImplementedError
